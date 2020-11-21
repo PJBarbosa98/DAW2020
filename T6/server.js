74528d462f0ca461e7776173ea1258a8513735dc
pjbarbosa98@gmail.com <pjbarbosa98@gmail.com>
2020-11-21 15:15:31 +0000

    tpc6 - post bug

--- T6/server.js
+++ T6/server.js
@@ -2,7 +2,9 @@ var http 	= require('http');
 var axios 	= require('axios');
 var fs 		= require('fs');
 
-var static 	= require('./static');
+var templates 	= require('./templates');
+var static 		= require('./static');
+var {parse} 	= require('querystring');
 
 /********************************************************************
  *
@@ -12,6 +14,35 @@ var static 	= require('./static');
  * json-server --watch db.json -p 3001
  *
  ********************************************************************/
+
+
+/********************************************************************
+ *
+ * Auxiliary Functions
+ *
+ ********************************************************************/
+
+/* recoverInfo : retrieves task info from request body */
+function recoverInfo ( request, callback )
+{
+	if (request.headers['content-type'] == 'application/x-www-form-urlencoded') {
+		let body = '';
+		request.on('data', bloco => {
+			body += bloco.toString();
+		})
+		request.on('end', () => {
+			console.log(body);
+			callback(parse(body));
+		})
+	}
+}
+
+/********************************************************************
+ *
+ * Create Server
+ *
+ ********************************************************************/
+
 
 var todo_server = http.createServer(function (req, res) {
 
@@ -28,7 +59,7 @@ var todo_server = http.createServer(function (req, res) {
 			if (static.recursoEstatico(req))
 			{
 				static.sirvoRecursoEstatico(req, res);
-				console.log('serving static resource: ' + req.url);
+				//console.log('Serving static resource: ' + req.url);
 			}
 
 
@@ -36,54 +67,123 @@ var todo_server = http.createServer(function (req, res) {
 			if (req.url == '/')
 			{
 				res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-
-				res.write(
-					`
-					<!DOCTYPE html>
-					<html>
-					<head>
-						<title>Goals List - Welcome</title>
-						<link rel="icon" href="favicon.ico"/>
-						<link rel="stylesheet" href="w3.css"/>
-					</head>
-					<body class="w3-lime">
-						<div class="w3-container w3-center" style="margin-top: 10%;">
-							<h2 class="w3-lime">Goals List</h2>
-							<h3 class="w3-lime">Welcome</h3>
-							<br><br>
-							
-							<div
-								class="w3-indigo w3-panel w3-border w3-round-large"
-								style="width: 25% !important; margin: auto;"
-							>
-								<p>Check your <a href="/tasks">Goals!</a></p>
-							</div>
-						</div>
-					</body>
-					</html>
-					`
-				);
-
+				res.write(templates.welcome_page());
 				res.end();
 			}
 
 			// GET tasks/
 			else if (req.url == '/tasks')
 			{
-				res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-				res.write('<h2>TASKSSSSSS Under Construction...</h2>');
-				res.write('<p>The page you requested has not been built yet!</p>');
-				res.end();	
+				axios.get('http://localhost:3001/tasks')
+				.then(response => {
+
+					var tasks = response.data;
+
+					res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+					res.write(templates.generate_tasks_page(tasks, d));
+					res.end();
+
+				})
+				.catch(error => {
+					res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+					res.write("<p>Failed to get tasks list!</p>");
+					res.end();
+				})
+			}
+
+			// GET tasks/{id}
+			// Fetch individual task page given its `id` value
+			// `id` must be of shape `t{number}`
+			if (/\/tasks\/(t)[0-9]+$/.test(req.url))
+			{
+				// Request URL split by ('/')
+				let parts 	= req.url.split('/');
+				// Task ID (retrieved from `parts`)
+				let t_id 	= parts[parts.length - 1];
+				
+				axios.get('http://localhost:3001/tasks/' + t_id)
+				.then(response => {
+
+					var task = response.data;
+
+					res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+					res.write(templates.generate_individual_task_page(task));
+					res.end();
+				})
+				.catch(error => {
+					res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+					res.write("<p>Failed to get task!</p>");
+					res.end();
+				});
+			}
+
+			// GET tasks/edit/{id}
+			// Edit task with id `id`
+			if (/\/tasks\/edit\/(t)[0-9]+$/.test(req.url))
+			{
+
+				// Request URL split by ('/')
+				let parts 	= req.url.split('/');
+				// Task ID (retrieved from `parts`)
+				let t_id 	= parts[parts.length - 1];
+
+				axios.get('http://localhost:3001/tasks/' + t_id)
+				.then(response => {
+
+					var task = response.data;
+
+					res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+					res.write(templates.edit_task_form(task));
+					res.end();
+				})
+				.catch(error => {
+					res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+					res.write("<p>Failed to edit task!</p>");
+					res.end();
+				});
 			}
 
 			break;
 
 		case "POST":
-			res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-			res.write('<h2>Under Construction...</h2>');
-			res.write('<p>The page you requested has not been built yet!</p>');
+			
+			recoverInfo(req, result => {
+				//console.log('TASK Post: ' +  JSON.stringify(result));
+				// axios.put??
+				axios.post('http://localhost:3001/tasks', result)
+				.then(resp => {
+					res.writeHead(200, {'Content-Type': 'text/html;charset=ut-8'});
+					res.write(templates.generate_post_confirm());
+					res.end();
+				})
+				.catch(erro => {
+					res.writeHead(200, {'Content-Type': 'text/html;charset=ut-8'});
+					res.write('<p>POST request failed!</p>');
+					res.end();
+				})
+			})
+
 			res.end();
 			break;
+/*
+ 		res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})
+            // Replace this code with a POST request to the API server
+            recuperaInfo(req, resultado => {
+                console.log('POST de aluno:' + JSON.stringify(resultado))
+                axios.post('http://localhost:3000/alunos', resultado)
+                    .then(resp => {
+                        res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})
+                        res.write(geraPostConfirm( resp.data, d))
+                        res.end()
+                    })
+                    .catch(erro => {
+                        res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})
+                        res.write('<p>Erro no POST: ' + erro + '</p>')
+                        res.write('<p><a href="/">Voltar</a></p>')
+                        res.end()
+                    })
+            })
+*/
 
 		case "PUT":
 			res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
@@ -113,3 +213,5 @@ var todo_server = http.createServer(function (req, res) {
 
 // Run server
 todo_server.listen(4000);
+
+console.log('Server running @ port 4000\nAccess: http://localhost:4000/');
---
+++ T6/templates.js
@@ -0,0 +1,338 @@
+/*
+ * `templates.js`
+ *
+ * Server module that holds HTML pages
+ *
+ */
+
+var fs     = require('fs');
+
+/* welcome_page: template for the welcome page */
+function welcome_page ()
+{
+  return `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Goals List - Welcome</title>
+    <meta charset="utf-8"/>
+    <link rel="icon" href="favicon.ico"/>
+    <link rel="stylesheet" href="w3.css"/>
+  </head>
+  <body class="w3-lime">
+    <div class="w3-container w3-center" style="margin-top: 10%;">
+      <h2 class="w3-lime">Goals List</h2>
+      <h3 class="w3-lime">Welcome</h3>
+      <br><br>
+      
+      <div
+        class="w3-indigo w3-panel w3-border w3-round-large"
+        style="width: 25% !important; margin: auto;"
+      >
+        <p>Check your <a href="/tasks">Goals!</a></p>
+      </div>
+    </div>
+  </body>
+  </html>
+  `
+}
+
+exports.welcome_page = welcome_page;
+
+/* generate_tasks_page: template for all tasks */
+function generate_tasks_page ( tasks, d )
+{
+  let html_page = `
+
+    <html>
+      
+      <head>
+        <title>Goals List - Tasks</title>
+        <meta charset="utf-8"/>
+        <link rel="icon" href="favicon.ico"/>
+        <link rel="stylesheet" href="w3.css"/>
+      </head>
+
+      <body>
+        <div class="w3-container w3-lime">
+          <h2 class="w3-center">Goals List</h2>
+          <pre class="w3-center">Session: ${d}</pre>
+        </div>
+
+        <div class="w3-row" style="margin-top: 5%;">
+          <!-- Ongoing Tasks -->
+          <div class="w3-col s6 w3-indigo w3-center">
+            <p class="w3-indigo">Ongoing Tasks</p>
+
+            <table class="w3-table w3-bordered w3-center w3-indigo">
+              <tr>
+                <th>ID</th>
+                <th>Description</th>
+              </tr>
+  `
+
+  tasks.forEach(t => {
+
+    if (t.status == 'Ongoing')
+    {
+      html_page += `
+        <tr>
+          <td>${t.id}</td>
+          <td><a href="tasks/${t.id}">${t.what}</a></td>
+          <td><a href="/tasks/edit/${t.id}">EDIT</a></td>
+          <td>DELETE</td>
+        </tr>
+      `;
+    }
+
+  });
+
+  html_page += `
+            </table>
+
+            <!-- Add new task -->
+            <div style="margin: auto; margin-top: 15%; width: 30% !important;"
+              class="w3-lime w3-panel w3-border w3-round-large"
+              style="width: 25% !important; margin: auto;"
+            >
+              <p>New Task</p>
+            </div>
+
+          </div>
+          <!-- Completed Tasks -->
+          <div class="w3-col s6 w3-blue w3-center">
+            <p class="w3-blue">Completed Tasks</p>
+
+            <table class="w3-table w3-bordered w3-center w3-blue">
+              <tr>
+                <th>ID</th>
+                <th>Description</th>
+              </tr>
+  `
+
+  tasks.forEach(t => {
+
+    if (t.status == 'Completed')
+    {
+      html_page += `
+        <tr>
+          <td>${t.id}</td>
+          <td><a href="/tasks/${t.id}">${t.what}</a></td>
+          <td><a href="/tasks/edit/${t.id}">EDIT</a></td>
+          <td>DELETE</td>
+        </tr>
+      `;
+    }
+
+  });
+
+
+  html_page += `
+            </table>
+
+            <!-- Add new task -->
+            <div style="margin: auto; margin-top: 15%; width: 30% !important;"
+              class="w3-lime w3-panel w3-border w3-round-large"
+              style="width: 25% !important; margin: auto;"
+            >
+              <p>Clear Completed</p>
+            </div>
+
+          </div>
+        </div> 
+      </body>
+    </html>
+
+  `;
+
+  return html_page;
+}
+
+exports.generate_tasks_page = generate_tasks_page;
+
+
+/* generate_individual_task_page : template for individual page */
+function generate_individual_task_page ( task )
+{
+  let html_page = `
+    <html>
+
+      <head>
+          <title>Goals List - ${task.id}</title>
+          <meta charset="utf-8"/>
+          <link rel="icon" href="favicon.ico"/>
+          <link rel="stylesheet" href="w3.css"/>
+      </head>
+
+      <body>
+        <div class="w3-container w3-lime">
+          <h2 class="w3-center">Task Page (${task.id})</h2>
+        </div>
+
+        <div style="margin-top: 5%;" class="w3-container w3-center"
+          <div class="w3-row">
+            
+            <div class="w3-col s6 w3-center">
+              <ul class="w3-ul w3-center w3-hoverable">
+                <li><b>ID</b></li>
+                <li><b>Date Created</b></li>
+                <li><b>Date Dued</b></li>
+                <li><b>Who</b></li>
+                <li><b>What</b></li>
+                <li><b>Type</b></li>
+                <li><b>Status</b></li>
+              </ul>
+            </div>
+            
+            <div class="w3-col s6 w3-center">
+              <ul class="w3-ul w3-center w3-hoverable">
+                <li>${task.id}</li>
+                <li>${task.dateCreated}</li>
+                <li>${task.dateDued}</li>
+                <li>${task.who}</li>
+                <li>${task.what}</li>
+                <li>${task.type}</li>
+                <li>${task.status}</li>                
+              </ul>
+            </div>
+          </div> 
+        </div>
+
+        <br/>
+        <div
+          class="w3-indigo w3-panel w3-border w3-round-large"
+          style="width: 25% !important; margin: auto; margin-top: 5%"
+        >
+          <p class="w3-center"><a href="/tasks">Go Back</a></p>
+        </div>
+
+      </body>
+
+    </html>
+  `;
+
+  return html_page;
+}
+
+exports.generate_individual_task_page = generate_individual_task_page;
+
+/* edit_task_form : template for task alteration page */
+function edit_task_form ( task )
+{
+  let html_page = `
+    <html>
+
+      <head>
+          <title>Task Edit - ${task.id}</title>
+          <meta charset="utf-8"/>
+          <link rel="icon" href="favicon.ico"/>
+          <link rel="stylesheet" href="w3.css"/>
+      </head>
+
+      <body>
+
+        <div class="w3-container w3-lime">
+          <h2 class="w3-center">Edit Task (${task.id})</h2>
+        </div>
+
+        <br/><br/>
+        <div class="w3-container w3-center">
+          <form class="w3-container" action="/tasks" method="POST">
+            <label class="w3-text-indigo"><b>ID</b></label>
+            <input style="width:50%; margin: auto; margin-top: 1%;"
+            class="w3-input w3-border w3-light-grey w3-center" type="text"
+            name="id" value="${task.id}">
+
+            <br/>
+            <label class="w3-text-indigo"><b>Date Created</b></label>
+            <input style="width:50%; margin: auto; margin-top: 1%;"
+            class="w3-input w3-border w3-light-grey w3-center" type="text"
+            name="dateCreated" value="${task.dateCreated}">
+
+            <br/>
+            <label class="w3-text-indigo"><b>Date Dued</b></label>
+            <input style="width:50%; margin: auto; margin-top: 1%;"
+            class="w3-input w3-border w3-light-grey w3-center" type="text"
+            name="dateDued" value="${task.dateDued}">
+
+            <br/>
+            <label class="w3-text-indigo"><b>Who has to do it</b></label>
+            <input style="width:50%; margin: auto; margin-top: 1%;"
+            class="w3-input w3-border w3-light-grey w3-center" type="text"
+            name="who" value="${task.who}">
+
+            <br/>
+            <label class="w3-text-indigo"><b>What is it</b></label>
+            <input style="width:50%; margin: auto; margin-top: 1%;"
+            class="w3-input w3-border w3-light-grey w3-center" type="text"
+            name="what" value="${task.what}">
+
+            <br/>
+            <label class="w3-text-indigo"><b>Task Type</b></label>
+            <input style="width:50%; margin: auto; margin-top: 1%;"
+            class="w3-input w3-border w3-light-grey w3-center" type="text"
+            name="type" value="${task.type}">
+
+            <br/>
+            <label class="w3-text-indigo"><b>Status</b></label>
+            <input style="width:50%; margin: auto; margin-top: 1%;"
+            class="w3-input w3-border w3-light-grey w3-center" type="text"
+            name="status" value="${task.status}">
+
+            <br/>
+            <input class="w3-btn w3-blue-grey" type="submit" value="Update"/>
+          </form>
+        </div>
+
+      </body>
+
+    </html>
+  `;
+
+  return html_page;
+}
+
+exports.edit_task_form = edit_task_form;
+
+/* generate_post_confirm: POST confirmation HTML page template */
+function generate_post_confirm ()
+{
+  return `
+    <html>
+      <head>
+          <title>POST receipt - ${task.id}</title>
+          <meta charset="utf-8"/>
+          <link rel="icon" href="favicon.ico"/>
+          <link rel="stylesheet" href="w3.css"/>
+      </head>
+      <body>
+        <div class="w3-card-4">
+          
+          <header class="w3-container w3-lime">
+            <h1>Task inserted!</h1>
+          </header>
+
+          <div
+            class="w3-indigo w3-panel w3-border w3-round-large"
+            style="width: 25% !important; margin: auto; margin-top: 5%"
+          >
+            <p class="w3-center"><a href="/tasks">Go Back</a></p>
+          </div>
+
+        </div>
+      </body>
+    </html>
+  `;
+}
+
+exports.generate_post_confirm = generate_post_confirm;
+
+/*
+      "id": "t1",
+      "dateCreated": "20/11/2020",
+      "dateDued": "21/11/2020",
+      "who": "Paulo Barbosa",
+      "what": "Task 1",
+      "type": "Task Type 1",
+      "status": "Ongoing"
+*/
